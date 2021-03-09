f42aa780b2cba6c2e1280ffe3d80964b963b9b6a
Rebecca Shao <yis324@lehigh.edu>
2021-03-09 20:48:10 +0800

    Assignment 03B Script

--- Assignment 03B.R
+++ Assignment 03B.R
@@ -3,12 +3,11 @@ results <- read.csv("PRESIDENT_precinct_primary.csv")
 n <- length(results$precinct)
 biden <- 0
 trump <- 0
-for (i in 2:n) {
+for (i in 1:n) {
   if (results$candidate[i] == "JOSEPH R BIDEN") {
     biden <- biden + results$votes[i]
   } else if (results$candidate[i] == "DONALD J TRUMP") {
     trump <- trump + results$votes[i]
   }
 }
-
 cat("Trump: ", trump, "\nBiden: ", biden)
